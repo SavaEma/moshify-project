7000a1a3cb70f92704eb92902d87bb34b3ddc45d
Ema Sava <savaema97@gmail.com>
2024-02-22 13:52:38 +0200

    create the collapsibles component

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+
+collapsibles.forEach((element) => {
+  element.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  });
+});
